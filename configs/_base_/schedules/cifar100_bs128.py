4ba3d817a543451ef2aa13ee8e635acfcc621a05
#ZHANG SHIQI# <qwert556@live.cn>
2022-11-14 14:24:33 +0800

    vis lr

--- configs/_base_/schedules/cifar100_bs128.py
+++ configs/_base_/schedules/cifar100_bs128.py
@@ -4,3 +4,5 @@ optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(policy='step', step=[100, 150])
 runner = dict(type='EpochBasedRunner', max_epochs=200)
+
+# python mmdl/tools/visualizations/vis_lr.py mmdl/configs/mobilenet_v3/bsconvu_mobilenet_v3_small_b128_cifar100.py --save-path ./mmdl/logs/lr_vis.png
