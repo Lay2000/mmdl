94b28f36c2e87055b35414a2b142b32f7d4c2eb1
unknown <982993741@qq.com>
2022-11-20 23:01:00 +0800

    'Add_Warmup_Configs_and_Result_Figures'

---
+++ configs/mobilenet_v3/mobilenet_v3_large_b128_cifar10_warmup.py
@@ -0,0 +1,8 @@
+_base_ = [
+    '../_base_/models/mobilenet_v3_large_cifar10.py',
+    '../_base_/datasets/cifar10_bs128.py',
+    '../_base_/schedules/cifar10_bs128_warmup.py', '../_base_/default_runtime.py'
+]
+
+lr_config = dict(policy='poly', power=0.7, min_lr=1e-4, by_epoch=False, warmup='exp', warmup_ratio=0.1, warmup_iters=5, warmup_by_epoch=True)
+runner = dict(type='EpochBasedRunner', max_epochs=200)
